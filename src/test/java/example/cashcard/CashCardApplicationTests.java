31d93e3ba582bd44d5c91f45b4e766a750ed5e76
Anupama <anupama80286@gmail.com>
2023-12-15 15:46:22 +0530

    add test to verify that, should not update a Cash Card owned by someone else

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -177,4 +177,16 @@ class CashCardApplicationTests {
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 
 	}
+
+	@Test
+	void shouldNotUpdateACashCardThatIsOwnedBySomeoneElse() {
+		CashCard kumarsCard = new CashCard(null, 500.00, null);
+		HttpEntity<CashCard> request = new HttpEntity<>(kumarsCard);
+
+		ResponseEntity<Void> response = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.exchange("/cashcards/102", HttpMethod.PUT, request, Void.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
 }
